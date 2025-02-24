b0b403528f6e27dd2b4a59a7d7f24f67fd09fabb
HoaiThanhCows <thothienti2@gmail.com>
2025-02-24 14:06:35 +0700

    Add project files.

---
+++ ThiTracNghiemTrucTuyen.Api/Data/Entities/User.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ThiTracNghiemTrucTuyen.Api.Data.Entities
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public int Name { get; set; }
+        [MaxLength(100)]
+        public int Email { get; set; }
+        [Length(9,15)]
+        public int Phone { get; set; }
+        [MaxLength(250)]
+        public int PasswordHash { get; set; }
+        [MaxLength(10)]
+        public string Role { get; set; } = nameof(UserRole.Student);
+
+
+         
+    }
+}
